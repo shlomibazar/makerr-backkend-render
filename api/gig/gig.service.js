e8b98920e41538df4dfc1509aa0c378b1388014d
Shlomi Bazar <shlomibazz@gmail.com>
2022-12-10 23:20:08 +0200

    modified:   api/gig/gig.service.js

--- api/gig/gig.service.js
+++ api/gig/gig.service.js
@@ -8,22 +8,9 @@ const ObjectId = require('mongodb').ObjectId
 async function query(filterBy = { txt, budget: { minPrice: 1, maxPrice: 10 }, label: '' }, sortBy) {
     console.log(filterBy);
     try {
-        // const criteria = {
-        //     title: { $regex: filterBy.txt, $options: 'i' }
-        // }
-
         const criteria = _buildCriteria(filterBy)
-        console.log('criteriaaaaaa',criteria)
         const collection = await dbService.getCollection('gig')
-        // if (criteria.title){
         var gigs = await collection.find(criteria).toArray()
-        // }
-        // if (criteria.budget){
-        //     var gigs = await collection.find(criteria.price)
-        //     // var gigs = await collection.find({criteria.budget})
-        // }
-        // const gigs = collection.find({ buyerId: id }).toArray()
-
         return gigs
     } catch (err) {
         logger.error('cannot find gigs', err)
@@ -101,16 +88,14 @@ async function removeGigMsg(gigId, msgId) {
         throw err
     }
 }
+
+
+////////////////////Backend /////////////////////
+
 function _buildCriteria(filterBy) {
     var criteria = {}
-    console.log('filterBy sss',filterBy)
-    // by text
-    // if(filterBy.txt){
-    //     const {title} = JSON.parse(filterBy.txt)
-    //     const regex = new RegExp(title,'i')
-    //     criteria.title = { $regex: regex }
-    //     console.log('filterBy.txt',filterBy.txt)
-    // }
+
+    //by title 
     criteria.title = { $regex: filterBy.txt, $options: 'i' }
 
     //by budget
@@ -118,28 +103,31 @@ function _buildCriteria(filterBy) {
     if (filterBy.budget?.minPrice) {
         criteria.price = { $gte: filterBy.budget.minPrice, $lte: filterBy.budget.maxPrice }
     }
-
-    //by delivert time 
+    //by delivery time 
     filterBy.daysToMake = JSON.parse(filterBy.daysToMake)
-    console.log('filterBy.delTime',filterBy.daysToMake)
     if (filterBy.daysToMake) {
         criteria.daysToMake = { $lte: filterBy.daysToMake }
     }
-
     // filter by labels
-    // filterBy.label = JSON.parse(filterBy.label)
     if (filterBy.label){
-        console.log('filterBy.labeleeeee',filterBy.label)
             criteria.labels = { $in: [filterBy.label] }
     }
-    // console.log('filterBy.labels',filterBy.labels)
-    // if (filterBy.labels?.length) {
-    // }
-    console.log('criteria in the end',criteria)
     return criteria
 
 }
   
+////////////////////Backend /////////////////////
+
+
+
+
+
+
+
+
+
+
+
 
 module.exports = {
     remove,
